47f86af6433aedd7099b67b2a3a874d247bb6402
David Han <dh4031@yahoo.com>
2015-03-19 22:41:36 +0000

    Version 1.3 - improved code for single fasta output with lowercase

--- fasta_answers/fasta_script.py
+++ fasta_answers/fasta_script.py
@@ -1,40 +1,17 @@
 import re #import regular expressions library
 import glob #import glob module to read multiple file pathnames
-import fileinput
 
-#this is to get the header and sequences from the file name
-def parse_fasta(filename):#opens and closes the file when it is done
-#lists multiple pathnames of all the files
-	path_names = glob.glob('../fasta_problem/FEC[0-9][0-9][0-9][0-9][0-9]_[0-9].seq')
-	for fasta in filename:
-		with open(fasta) as seqfile:
-			for line in seqfile:
-				line = line.strip() #removes newline
-				if line.startswith(">"): #creates header
-					name = line
-					print name
-				else:
-					seqs = line.lower() #creates the sequences and puts them into lowercase
-					print seqs
-					seqs_output = open("single_file_fasta_format.fa", "w")
-					seqs_output.write(seqs)
-					seqs_output.close()
+#creates one output from multiple files
+def parse_fasta2(filename):#
+	with open('single_output_fasta.fa','w') as outfile: #creates a new file ready to write
+		for fasta in path_names: #each path name into fasta
+			with open(fasta) as infile: #opens up seq file
+				for line in infile: 
+					if line.startswith(">"):
+						outfile.write(line)
+					else:
+						outfile.write(line.lower()) #writes the output of each file
+	print 'A single output file called single_output_fasta.fa has successfully created.'
 
-def parse_fasta2(filename):#opens and closes the file when it is done
-#lists multiple pathnames of all the files
-	path_names = glob.glob('../fasta_problem/FEC[0-9][0-9][0-9][0-9][0-9]_[0-9].seq')
-	with open('output_test.fa','w') as outfile:
-		for fasta in path_names:
-			with open(fasta) as infile:
-				for line in infile:
-					outfile.write(line)
-
-path_names = glob.glob('../fasta_problem/FEC[0-9][0-9][0-9][0-9][0-9]_[0-9].seq') #lists multiple pathnames of all the files
-for output in path_names:
-	print output
-
-def output_fasta():
-	path_names = glob.glob('../fasta_problem/FEC[0-9][0-9][0-9][0-9][0-9]_[0-9].seq') #lists multiple pathnames of all the files
-	parse_fasta(path_names)
-	for output in fileinput.input(path_names):
-		print output
+path_names = glob.glob('../fasta_problem/FEC[0-9][0-9][0-9][0-9][0-9]_[0-9].seq')
+parse_fasta2(path_names)
