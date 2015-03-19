8056fe07685dfba201bbc31b095d35f440f5219c
David Han <dh4031@yahoo.com>
2015-03-19 20:58:42 +0000

    Version 1.2 - parse_fasta2 - prevents duplication of output

--- fasta_answers/fasta_script.py
+++ fasta_answers/fasta_script.py
@@ -4,6 +4,8 @@ import fileinput
 
 #this is to get the header and sequences from the file name
 def parse_fasta(filename):#opens and closes the file when it is done
+#lists multiple pathnames of all the files
+	path_names = glob.glob('../fasta_problem/FEC[0-9][0-9][0-9][0-9][0-9]_[0-9].seq')
 	for fasta in filename:
 		with open(fasta) as seqfile:
 			for line in seqfile:
@@ -12,9 +14,20 @@ def parse_fasta(filename):#opens and closes the file when it is done
 					name = line
 					print name
 				else:
-					seqs = line #creates the sequences
+					seqs = line.lower() #creates the sequences and puts them into lowercase
 					print seqs
+					seqs_output = open("single_file_fasta_format.fa", "w")
+					seqs_output.write(seqs)
+					seqs_output.close()
 
+def parse_fasta2(filename):#opens and closes the file when it is done
+#lists multiple pathnames of all the files
+	path_names = glob.glob('../fasta_problem/FEC[0-9][0-9][0-9][0-9][0-9]_[0-9].seq')
+	with open('output_test.fa','w') as outfile:
+		for fasta in path_names:
+			with open(fasta) as infile:
+				for line in infile:
+					outfile.write(line)
 
 path_names = glob.glob('../fasta_problem/FEC[0-9][0-9][0-9][0-9][0-9]_[0-9].seq') #lists multiple pathnames of all the files
 for output in path_names:
