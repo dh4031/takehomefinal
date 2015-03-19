dcaabe68a6cd36770668e72fa9c01f7d1a93ecfe
David Han <dh4031@yahoo.com>
2015-03-19 09:18:00 +0000

    Version 1.1 - updated output method

--- fasta_answers/fasta_script.py
+++ fasta_answers/fasta_script.py
@@ -4,8 +4,8 @@ import fileinput
 
 #this is to get the header and sequences from the file name
 def parse_fasta(filename):#opens and closes the file when it is done
-	for hi in filename:
-		with open(hi) as seqfile:
+	for fasta in filename:
+		with open(fasta) as seqfile:
 			for line in seqfile:
 				line = line.strip() #removes newline
 				if line.startswith(">"): #creates header
@@ -14,17 +14,6 @@ def parse_fasta(filename):#opens and closes the file when it is done
 				else:
 					seqs = line #creates the sequences
 					print seqs
-
-def parse_fasta2(filename):#opens and closes the file when it is done
-	for line in filename:
-		line = line.strip() #removes newline
-		if line.startswith(">"): #creates header
-			name = line
-			print name
-		else:
-			seqs = line #creates the sequences
-			print seqs1
-
 
 
 path_names = glob.glob('../fasta_problem/FEC[0-9][0-9][0-9][0-9][0-9]_[0-9].seq') #lists multiple pathnames of all the files
