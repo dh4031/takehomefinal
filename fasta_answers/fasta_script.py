9ffa684e0425b74ff2e95638041980af28654f1f
David Han <dh4031@yahoo.com>
2015-03-20 23:13:53 +0000

    updated fasta function with output file

--- fasta_answers/fasta_script.py
+++ fasta_answers/fasta_script.py
@@ -6,12 +6,47 @@ def parse_fasta2(filename):#
 	with open('single_output_fasta.fa','w') as outfile: #creates a new file ready to write
 		for fasta in path_names: #each path name into fasta
 			with open(fasta) as infile: #opens up seq file
-				for line in infile: 
+				for line in infile: #prevents from overwriting 
 					if line.startswith(">"):
 						outfile.write(line)
 					else:
-						outfile.write(line.lower()) #writes the output of each file
-	print 'A single output file called single_output_fasta.fa has successfully created.'
+						outfile.write(line.lower().strip()) #writes the output of each file
+	
+print 'A single output file called single_output_fasta.fa has successfully created.'
 
 path_names = glob.glob('../fasta_problem/FEC[0-9][0-9][0-9][0-9][0-9]_[0-9].seq')
 parse_fasta2(path_names)
+
+def log_file(filename):
+	with open('log_file.txt','w') as outfile:
+		outfile.write("sequence_header" + "," +  "length" + "\n")
+		for fasta in path_names:
+			with open(fasta) as infile:
+				for line in infile:
+					if line.startswith(">"):
+						outfile.write(line.strip() + ",")
+					else:
+						hi = line.strip()
+						outfile.write(hi)
+
+def parse_fasta(filename):
+	for fasta in path_names:
+		with open(fasta) as seqfile:
+			name = ""
+			seqs = ""
+			for line in seqfile:
+				line = line.strip()
+				if line.startswith(">"):
+					if name != "":
+						yield name, seqs
+						seqs = ""
+					name = line.lstrip(">")
+				else: 
+					seqs = seqs + line
+
+for name, sequence in parse_fasta(path_names):
+    print "{} is {} base pairs long".format(name, len(sequence))
+
+parse_fasta(path_names)
+
+
